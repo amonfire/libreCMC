6f83f32e99c812087f39ec1bc46fa4078da8a218
RISCi_ATOM <bob@bobcall.me>
2017-03-13 01:10:50 -0400

    add libreCMC src repository to scripts/download.pl

--- scripts/download.pl
+++ scripts/download.pl
@@ -266,6 +266,7 @@ foreach my $mirror (@ARGV) {
 }
 
 #push @mirrors, 'http://mirror1.openwrt.org';
+push @mirrors, 'https://librecmc.org/librecmc/downloads/sources/v1.4/';
 push @mirrors, 'http://sources.lede-project.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
