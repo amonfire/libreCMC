0b4c2310200fc8f02468361de901d7d59bd9622d
RISCi_ATOM <bob@bobcall.me>
2017-03-23 18:07:42 -0400

    Update commit commit hash in scripts/getver.sh

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -12,7 +12,7 @@ try_version() {
 }
 
 try_git() {
-	REBOOT=ee53a240ac902dc83209008a2671e7fdcf55957a
+	REBOOT=b1426a23c685ce07995988682b5421556f104367
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
 
 	[ -n "$GET_REV" ] || GET_REV="HEAD"
